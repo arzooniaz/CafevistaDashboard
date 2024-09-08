962bcd0b16064cea1be8080cdad2c2e8c3e61a42
Rizwikhattak <rizwikhattak4384@gmail.com>
2024-09-08 13:56:48 +0500

    trying to merge

--- src/Employees/Layout/Imports.js
+++
@@ -1,8 +0,0 @@
-// export { default as EmployeeLayout } from "./EmployeeLayout";
-export { default as ChattingPage } from "../Components/Chatting/ChatingPage";
-export { default as DashBoard } from "../Components/DashBoard/DashBoard";
-export { default as Logout } from "../Components/Logout/Logout";
-export { default as Pricing } from "../Components/Pricing/Pricing";
-export { default as Projects } from "../Components/Projects/Projects";
-export { default as Settings } from "../Components/Settings/Settings";
-export { default as Team } from "../Components/Team/Team";
